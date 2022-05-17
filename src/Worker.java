18b121faa0dc5a8ed5f5e8681d306fb46ee12544
Юрий Данильченко <genitra111@gmail.com>
2022-05-17 18:15:12 +1000

    Внесены изменения в функционал программы

--- src/Worker.java
+++ src/Worker.java
@@ -3,14 +3,17 @@ package src;
 public class Worker {
 
     private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener callback) {
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
+        this.errorCallback = errorCallback;
     }
 
     public void start() {
         int errorTask = 33;
         for (int i = 0; i < 100; i++) {
+            if (i == errorTask) errorCallback.onError("Задача " + i + " не выполнена");
             callback.onDone("Задача " + i + " выполнена");
         }
     }
